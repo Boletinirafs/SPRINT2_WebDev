bc42ade69cae2b1cc24e534bf081e6d41e9d385a
Boletinirafs <rafa.bo.oliv@gmail.com>
2026-05-15 20:16:35 -0300

    Commit 4 - Trocar login/cadastro

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
+//funcao para alternar aberturas contrarias
+const trocar = (x) =>{
+    const troca = document.querySelector(x)
+    if (!troca.classList.contains('hide')){
+        troca.classList.toggle('hide')
+    }
+}
 //abre a aba de login
 const botaoLogin = document.querySelector('.login')
 botaoLogin.addEventListener('click',()=>{
+    trocar('.cadastroForm')
     const loginForm = document.querySelector('.loginForm')
     //alterna os valores internos
     loginForm.classList.toggle('hide')
 })
-
 //funcao para abrir e fechar o cadastro
 const botaoCadastro = document.querySelector('.cadastro')
 botaoCadastro.addEventListener('click',()=>{
